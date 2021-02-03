1e5f8fbc7664ae63c18a2c811ddc831abf4a9cff
ThatJacobo <thatjacobo@gmail.com>
2021-02-03 14:26:31 -0800

    beautify

--- des157/Studio1/script.js
+++ des157/Studio1/script.js
@@ -6,7 +6,7 @@
     const rst = document.querySelector('#rst');
     const nInsert = document.querySelector('#nInsert')
 
-    myForm.addEventListener('submit', function(event){
+    myForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
         document.querySelector('.hidden').className = 'bgDv showing'
@@ -22,18 +22,17 @@
         const concept = document.querySelector('#concept').value;
         const name = document.querySelector('#name').value;
         const fname = document.querySelector('#fname').value;
-        
-        if(noun1 && noun2 && adj && verb && food && concept && name && fname) {
+
+        if (noun1 && noun2 && adj && verb && food && concept && name && fname) {
             var myText = `<p id="userResponse" class="showing">One autumn evening at a mysterious <b>${food}</b> stand behind the <b>${name}</b> Shrine, a <b>${adj}</b> third-year college student bumps into a <b>${noun1}</b> with an <b>${noun2}</b>-shaped head who calls himself a god of <b>${concept}</b>. Meeting this <b>${noun1}</b> causes the student to reflect upon his past two years at college—two years bitterly spent trying to <b>${verb}</b> couples on campus with his only friend <b>${fname}</b><p>`;
-        }
-        else {
+        } else {
             var myText = "Please give me words so I can make your Mad Lib!"
         }
 
         nInsert.innerHTML = myText;
 
         const formData = document.querySelectorAll("input[type=text]");
-        for(eachField of formData) {
+        for (eachField of formData) {
             eachField.value = "";
         }
     });
@@ -43,7 +42,7 @@
         window.location.reload();
         /* knew this from previous experience, it made the quickest way to restart */
     })
-    
 
-    
+
+
 })();
